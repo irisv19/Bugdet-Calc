c7c3d95891bbe1ded0885960d0c84c96aae571ce
irisv19 <143832578+irisv19@users.noreply.github.com>
2024-03-08 10:39:54 -0800

    Almost done

---
+++ calc.js
@@ -0,0 +1,17 @@
+
+new Chart("myChart", {
+    type: "doughnut",
+    data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: "Total Expenses"
+        }
+    }
+});
